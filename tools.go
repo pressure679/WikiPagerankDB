811e5e3cbf86296faa7db861e3979b5c393e6297
Vittus Mikiassen <pressure679@users.noreply.github.com>
2017-07-10 13:20:23 -0200

    Update tools.go
    Added the rake function. it needs a final return argument in the NLP function, other than that the v.1 for producing new ghostwritten text based on the wikipaedia dump files just needs to utilize the given functions (arguments being article titles) and apply an A* algorithm or similar to traverse the articles' links. - Hmm, from here I planned to implement the naive bayes, tf-idf, and pagerank algorithms, where pagerank would definitely add an improvement. v.2 should communicate with a P2P network to share information (to save processing power) and make an interactive chat/learn bot or matrix-like terminal with keyword-based coloring. - There are also the translation option, rivescript, aiml for the keywords and sentence construction. And don't forget the perceptron based tagger if unknown writing styles or languages are included.

--- tools.go
+++ tools.go
@@ -1,29 +1,85 @@
-package myTools
+/*
+		Copyright (C) 2017 Vittus Mikiassen
+
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+
+		You should have received a copy of the GNU General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package GhostWriter
 import (
+	"fmt"
+	"os"
 	"io"
 	"io/ioutil"
-	"os"
-	"encoding/gob"
+	"bufio"
 	"strconv"
-	"fmt"
 	"bytes"
-	"encoding/xml"
 	"strings"
 	"regexp"
-	"github.com/nyxtom/viterbi"
+	"encoding/gob"
+	"encoding/xml"
+	"sort"
 	"github.com/Obaied/rake"
-	"github.com/jbrukh/bayesian"
+	// "github.com/dustin/go-wikiparse"
 )
+
+// Graciously open sources by github.com/dustin/go-wikiparse under an MIT styled license (10TH of July 2017).
+type SiteInfo struct {
+	SiteName   string `xml:"sitename"`
+	Base       string `xml:"base"`
+	Generator  string `xml:"generator"`
+	Case       string `xml:"case"`
+	Namespaces []struct {
+		Key   string `xml:"key,attr"`
+		Case  string `xml:"case,attr"`
+		Value string `xml:",chardata"`
+	} `xml:"namespaces>namespace"`
+}
+// A Contributor is a user who contributed a revision.
+type Contributor struct {
+	ID       uint64 `xml:"id"`
+	Username string `xml:"username"`
+}
+// A Redirect to another Page.
+type Redirect struct {
+	Title string `xml:"title,attr"`
+}
+// A Revision to a page.
+type Revision struct {
+	ID          uint64      `xml:"id"`
+	Timestamp   string      `xml:"timestamp"`
+	Contributor Contributor `xml:"contributor"`
+	Comment     string      `xml:"comment"`
+	Text        string      `xml:"text"`
+}
+// A Page in the wiki.
+type Page struct {
+	Title     string     `xml:"title"`
+	ID        uint64     `xml:"id"`
+	Redir     Redirect   `xml:"redirect"`
+	Revisions []Revision `xml:"revision"`
+	Ns        uint64     `xml:"ns"`
+}
 
 type Section struct {
 	Content []byte
 	References []byte
-	RakeCands *[]string
+	RakeCands rake.PairList
 }
 type LeafPR map[string][]byte
 type PageItems struct {
 	// Sections, title indicated by key, item is content/text
-	Sections map[string]Section
+	Sections map[string]*Section
 
 	// The NodeID, used for graphing/treeing
 	NodeID []byte
@@ -34,31 +90,43 @@ type PageItems struct {
 	// Links from this article, used to collect them for the MySQL Db, after that the program will use them to utilize the Db for Dijkstra's algorithm and the Pagerank algorithm.
 	Links []byte
 
-	Pageranks map[uint8]LeafPR
+	Pageranks map[uint8]*LeafPR
 
 	Index []byte
 }
 
 // These cross-reference each other for input/output correlation.
-type Tokens [][3]string
-type TokenChains struct {
+type TokenChains [][3]string
+/* type TokenChains struct {
 	Chains Tokens
-}
+} */
 // From: irc, freenode, #go-nuts, user: pestle, snippet: https://play.golang.org/p/F4ACtG_6cP, modified a bit.
-func (tc TokenChains) SortTokens() {
-	for num, _ := range tc {
-		sort.Slice(tc[num], func(i, j int) bool {
-			for k := 0; k < 3; k++ {
+func (tc TokenChains) Len() int { return len(tc) }
+func (tc TokenChains) Less(i, j int) bool {
+	for i, _ := range tc {
+		for k := 0; k < 3; k++ {
 				// This was not needed in my code.
-				if data[i][k] == data[j][k] {
+				if tc[i][k] == tc[j][k] {
 					continue
 				}
-				return data[i][k] < data[j][k]
+				return tc[i][k] < tc[j][k]
+		}
+	}
+	return false
+}
+func (tc TokenChains) Swap(i, j int) { tc[i], tc[j] = tc[j], tc[i] }
+/* func (tc TokenChains) SortTokens() {
+	for num, _ := range tc {
+		sort.Sort(tc[num], func(i, j int) bool {
+			for k := 0; k < 3; k++ {
+				// This was not needed in my code.
+				if tc[i][k] == tc[j][k] { continue }
+				return tc[i][k] < tc[j][k]
 			}
 			return false
 		})
 	}
-}
+} */
 
 func GetFilesFromArticlesDir(wikiDirectory string) (files []string, err error) {
 	osFileInfo, err := ioutil.ReadDir(wikiDirectory)
@@ -122,19 +190,31 @@ func ReadWikiIndices(readDirectory string, articles []string) (index map[string]
 	}
 	return
 }
-func ReadWikiXML(readDirectory string, filesIndices map[string][]byte) (articles map[string]PageItems, links []byte, err error) {
+func ReadWikiXML(readDirectory string, filesIndices map[string][]byte) (articles map[string]*PageItems, links []byte, err error) {
+	var xmlDecoder *xml.Decoder
 	files, err := GetFilesFromArticlesDir(readDirectory)
 	if err != nil { return nil, nil, err }
 	// sections = make(map[string][]byte)
-	page := wikiparse.Page{}
+	// page := wikiparse.Page{}
 	
 	buffer := new(bytes.Buffer)
 	encoder := gob.NewEncoder(buffer)
 	var bufferLinks string
+	articles = make(map[string]*PageItems)
+	// tmpSection := make(map[string]Section)
 
-	for _, xmlFile:= range files {
+	reTitle, err := regexp.Compile("[=]{2,5}.{1,50}[=]{2,5}")
+	if err != nil { return }
+	reReferences, err := regexp.Compile("<ref>\\w+</ref>")
+	if err != nil { return }
+	var page Page
+	
+	var section string
+	var counter uint8 = 0
+
+	for _, xmlFile := range files {
 		ioReader, err := os.Open(readDirectory + "/" + xmlFile)
-		decoder := xml.NewDecoder(ioReader)
+		xmlDecoder := xml.NewDecoder(ioReader)
 		if err != nil { return nil, nil, err }
 		
 		for _, offset := range filesIndices {
@@ -143,82 +223,86 @@ func ReadWikiXML(readDirectory string, filesIndices map[string][]byte) (articles
 			if err != nil { return nil, nil, err }
 			_, err = ioReader.Seek(start, io.SeekStart)
 			if err != nil { return nil, nil, err }
-
-			err = decoder.Decode(&page)
-			if err != nil { return nil, nil, err }
-
-			for i := 0; i < len(page.Revisions); i++ {
-				if page.Revisions[i].Text != "" {
-					articles[page.Title].Content, err = GetSections(page.Revisions[i].Text, page.Title)
-					if err != nil { return nil, nil, err }
-					tempLinks := wikiparse.FindLinks(page.Revisions[i].Text)
-					for num, link := range tempLinks {
-						bufferLinks += link
-						if num < len(tempLinks) { bufferLinks += "-" }
+			for {
+				err = xmlDecoder.Decode(&page)
+				if err != nil {
+					if strings.EqualFold(err.Error(), io.EOF.Error()) {
+						break
+					} else {
+						return nil, nil, err
 					}
-					err = encoder.Encode(bufferLinks)
-					if err != nil { return nil, nil, err }
-					articles[page.Title]["links"] = buffer.Bytes()
-					buffer.Reset()
+				}
+
+				if page.Revisions[0].Text != "" {
+					// articles[page.Title].Sections, err = GetSections(page.Revisions[i].Text, page.Title)
+					//
+					page.Revisions[0].Text = strings.Replace(page.Revisions[0].Text, "&lt", "<", -1)
+					page.Revisions[0].Text = strings.Replace(page.Revisions[0].Text, "&gt", "<", -1)
+					page.Revisions[0].Text = strings.Replace(page.Revisions[0].Text, "&quot", "\"", -1)
+					page.Revisions[0].Text = strings.Replace(page.Revisions[0].Text, ";", "", -1)
+
+					// TODO: Depending on the hardware limits/utilization use 1 loop for both regex searches.
+					refIndex := reReferences.FindAllStringIndex(page.Revisions[0].Text, -1)
+					titleIndex := reTitle.FindAllStringIndex(page.Revisions[0].Text, -1)
+					for i := 0; i < len(refIndex); i++ {
+						if refIndex[0] != nil {
+							if refIndex[0][0] > titleIndex[counter][0] {
+								counter++
+								section = page.Revisions[0].Text[
+									titleIndex[counter][0]:
+									titleIndex[counter][1]-1]
+							}
+							encoder.Encode([]byte(page.Revisions[0].Text[
+								refIndex[i][0]:
+								refIndex[1+1][0]]))
+							// TODO: implement the assignment of the sections struct in this loop and loop below this one.
+							// tmpSection[section].References = buffer.Bytes()
+							// articles[page.Title].Sections[section] = Section{[]byte{0}, []byte{0}}
+							/* if data, ok := articles[page.Title].Sections[section]; ok {
+								data.References = buffer.Bytes()
+								articles[page.Title].Sections[section] = data
+							} */
+							articles[page.Title].Sections[section].References = buffer.Bytes()
+						}
+						buffer.Reset()
+					}
+					page.Revisions[0].Text = reReferences.ReplaceAllString(page.Revisions[0].Text, "")
+					titleIndex = reTitle.FindAllStringIndex(page.Revisions[0].Text, -1)
+					for i := 0; i < len(titleIndex)-1; i++ {
+						if titleIndex[0] != nil {
+							if i == 0 {
+								encoder.Encode([]byte(page.Revisions[0].Text[:titleIndex[0][1]-1]))
+								/* if data, ok := articles[page.Title].Sections["Summary"]; ok {
+									data.Content = buffer.Bytes()
+									articles[page.Title].Sections["Summary"].Content = data.Content
+								} */
+								articles[page.Title].Sections["Summary"].Content = buffer.Bytes()
+							} else if i < len(titleIndex) - 1 {
+								encoder.Encode([]byte(page.Revisions[0].Text[
+									titleIndex[i][1]:
+									titleIndex[i+1][0]]))
+								articles[page.Title].Sections[page.Revisions[0].Text[
+									titleIndex[i][0]:
+									titleIndex[i][1]]].Content = buffer.Bytes()
+							} else {
+								encoder.Encode([]byte(page.Revisions[0].Text[
+									titleIndex[i][1]:
+									len(page.Revisions[0].Text)]))
+								articles[page.Title].Sections[page.Revisions[0].Text[
+									titleIndex[i][0]:
+									titleIndex[i][1]]].Content = buffer.Bytes()
+							}
+							buffer.Reset() // TODO: Depending on the CPU only reset this once and use another buffer for the gob encoder.
+						}
+					}
+					//
 				}
 			}
 		}
 	}
 	return articles, links, nil
 }
-func GetSections(page, title string) (Content PageItems, err error) {
-	page = strings.Replace(page, "&lt", "<")
-	page = strings.Replace(page, "&gt", "<")
-	page = strings.Replace(page, "&quot", "\"")
-	page = strings.Replace(page, ";", "")
 
-	// TODO: Depending on the hardware limits/utilization use 1 loop for both regex searches.
-	var lastRefIndex uint // TODO: If a lastRefIndex overflow error should occur increase the buffer length or algorithm to be sequential.
-	buffer := new(bytes.Buffer)
-	encoder := gob.NewEncoder(buffer)
-	reTitle, err := regexp.Compile("[=]{2,5}.{1,50}[=]{2,5}")
-	if err != nil { return }
-	reReferences, err := regexp.Compile("<ref>\\w+</ref>")
-	if err != nil { return }
-	for i := 0; i < len(refIndex); i++ {
-		if refIndex[i] != nil {
-			if i > 0 {
-				if refIndex[i][1] - 1 == refIndex[i - 1][0] - 1 {
-					encoder.Encode([]byte(page[:refIndex[i][1]-1]))
-					Content.Sections[[]byte(strconv.Itoa(refIndex[i][0] - 1))].References = buffer.Bytes()
-				} else {
-					lastRefIndex = refIndex[i][0]
-					encoder.Encode([]byte(page[:refIndex[i][1]-1]))
-					Content.Sections[[]byte(strconv.Itoa(refIndex[i][0] - 1))].References = buffer.Bytes()
-				}
-			} else {
-				// TODO: Might want to exchange i with 0.
-				encoder.Encode([]byte(page[:refIndex[i][1]-1]))
-				Content.Sections[[]byte(strconv.Itoa(refIndex[i][0] - 1))].References = buffer.Bytes()
-			}
-			page[refIndex[i][0] - 1:refIndex[i][1] - 1] = ""
-		}
-		buffer.Reset()
-	}
-	titleIndex := reTitle.FindAllStringIndex(page, -1)
-	for i := 0; i < len(titleIndex)-1; i++ {
-		refIndex := reReferences.FindAllStringIndex(titleIndex[i], -1)
-		if titleIndex[i] != nil {
-			if i == 0 {
-				encoder.Encode([]byte(page[:titleIndex[i][1]-1]))
-				Content.Sections["Summary"].Content = buffer.Bytes()
-			} else if i < len(titleIndex) - 1 {
-				encoder.Encode([]byte(page[titleIndex[i][1]:titleIndex[i+1][0]]))
-				Content.Sections[page[titleIndex[i][0]:titleIndex[i][1]]].Content = buffer.Bytes()
-			} else {
-				encoder.Encode([]byte(page[titleIndex[i][1]:len(page)]))
-				Content.Sections[page[titleIndex[i][0]:titleIndex[i][1]]].Content = buffer.Bytes()
-			}
-			buffer.Reset() // TODO: Depending on the CPU only reset this once and use another buffer for the gob encoder.
-		}
-	}
-	return
-}
 // This is implemented inside the GetSections function
 /* func StripXMLEntities(articles map[string]PageItems) {
 	for _, article := range articles {
@@ -229,23 +313,22 @@ func GetSections(page, title string) (Content PageItems, err error) {
 } */
 // TODO: convert the uint item from the chains map to []byte when it is determined how many occurrences of that specific chain there are.
 
-func LoadMarkovChain() (PTBTagMap map[string]TokenChains, WordMap map[string]TokenChains, err error) {
+// Load the markov chains tagged with the penn treebank tags, repository: https://nlp.stanford.edu/links/statnlp.html
+func LoadMarkovChain() (PTBTagMap map[string]*TokenChains, WordMap map[string]*TokenChains, err error) {
 	markovEntities := make(map[string]string)
 	osFileInfo, err := ioutil.ReadDir("dependency_treebank")
-	if err != nil { return nil, err }
+	if err != nil { return nil, nil, err }
 	var counter uint8 = 0
 	var isInMap bool = false
 	var buffer [2][3]string
 	
 	for _, file := range osFileInfo {
 		if !file.IsDir() {
-			files = append(files, file.Name())
-			osFile, err := os.Open("dependency_treebank/" + file)
-			if err != nil { return nil, err }
+			osFile, err := os.Open("dependency_treebank/" + file.Name())
+			if err != nil { return nil, nil, err }
 			defer osFile.Close()
 			bufioReader := bufio.NewReader(osFile)
 			for {
-				counter++
 				line, _, err  := bufioReader.ReadLine()
 				if err == io.EOF {
 					break
@@ -253,47 +336,49 @@ func LoadMarkovChain() (PTBTagMap map[string]TokenChains, WordMap map[string]Tok
 					return nil, nil, err
 				}
 				symbols := strings.Split(string(line), "	")
-				buffer[0] = append(buffer[0], symbols[0])
-				buffer[1] = append(buffer[1], symbols[1])
+				buffer[0][counter] = symbols[0]
+				buffer[1][counter] = symbols[1]
+				counter++
+	
+				if counter == 2 { counter = 0 }
 			}
 			for i := 0; i < len(buffer[0]) - 3; i++ {
-				PTBTagMap[buffer[1]].Chains = append(PTBTagMap[buffer[1]].Chains, buffer[0][i])
-				PTBTagMap[buffer[1]].Chains = append(PTBTagMap[buffer[1]].Chains, buffer[0][i + 1])
-				PTBTagMap[buffer[1]].Chains = append(PTBTagMap[buffer[1]].Chains, buffer[0][i + 2])
-				WordMap[buffer[0]].Chains = append(WordMap[buffer[0]].Chains, buffer[1][i])
-				WordMap[buffer[0]].Chains = append(WordMap[buffer[0]].Chains, buffer[1][i + 1])
-				WordMap[buffer[0]].Chains = append(WordMap[buffer[0]].Chains, buffer[1][i + 2])
+				// PTBTagMap[buffer[1][0]].Chains = append(PTBTagMap[buffer[1][0]].Chains, [3]string{buffer[i][i], buffer[i][i + 1], buffer[i][i + 2]})
+				*PTBTagMap[buffer[1][0]] = append(*PTBTagMap[buffer[1][0]], [3]string{buffer[i][i], buffer[i][i + 1], buffer[i][i + 2]})
+				// WordMap[buffer[i][0]].Chains = append(WordMap[buffer[i][0]].Chains, [3]string{buffer[1][i], buffer[1][i + 1], buffer[1][i + 2]})
+				*WordMap[buffer[i][0]] = append(*WordMap[buffer[i][0]], [3]string{buffer[1][i], buffer[1][i + 1], buffer[1][i + 2]})
 			}
 			for key, _ := range PTBTagMap {
-				PTBTagMap[key].SortTokens()
+				// PTBTagMap[key].SortTokens()
+				sort.Sort(PTBTagMap[key])
 			}
-			for key, _ := range PTBTagMap {
-				WordMap[key].SortTokens()
+			for key, _ := range WordMap {
+				// WordMap[key].SortTokens()
+				sort.Sort(WordMap[key])
 			}
 		}
 	}
 	return PTBTagMap, WordMap, nil
 }
 func NLP(articles map[string]PageItems) (text string, err error) {
-	indices, err := ReadWikiIndices("articles", articles)
+	// These 2 below functions should be called from the main function.
+	/* indices, err := ReadWikiIndices("articles", articles)
 	if err != nil { return nil, err }
-	articles, _, err := ReadWikiXML("articles", indices)
-	if err != nil { return nil, err }
+	articles, _, err = ReadWikiXML("articles", indices)
+	if err != nil { return nil, err } */
+	decBuffer := new(bytes.Buffer)
+	decoder := gob.NewDecoder(decBuffer)
+	encBuffer := new(bytes.Buffer)
+	encoder := gob.NewEncoder(encBuffer)
+	
 	PTBTagMap, WordMap,  err := LoadMarkovChain()
-	if err != nil { return nil, err }
-	// Rake and TF-IDF and produce text between input/output texts based on pageranked input.
-	/* const badTags []string = []string{"NN", "PRP", "WP", "EX"}
-	const NonWord []string = []string{ }
-	var badWords []string
-	for absBadTag := range badTags {
-		for _, absBadWord := range PTBTagMap[absBadWord].Chains {
-			badWords = append(badWords, absBadWord[0])
-		}
-	} */
+	if err != nil { return "", err }
 	for key, item := range articles {
-		for _, sectionTitle := range item.Sections {
-			*articles[key].Sections[sectionTitle].RakeCands = rake.RunRake(articles[key].Sections[sectionTitle].Content)
+		for sectionTitle, _ := range item.Sections {
+			decoder.Decode(articles[key].Sections[sectionTitle].Content)
+			articles[key].Sections[sectionTitle].RakeCands = rake.RunRake(string(decBuffer.Bytes()))
+			decBuffer.Reset()
 		}
 	}
+	
 }
-
