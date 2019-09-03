a843570b09ccc24d4b44c35cf0f6a98b8f780747
Vittus Mikiassen <vittusmpress@yahoo.com>
2019-09-03 12:57:56 -0200

    Create sample-linear-regression.go
    This is linear regression written in Golang, the data used can be replaced, but nonetheless they can be read and used to understand how and what linear regression does. The code is used in Rake, and Non-viterbi-process.

---
+++ sample-linear-regression.go
@@ -0,0 +1,34 @@
+func (graph *Graph) LinearRegression() {
+	graph.NounMinOccurence = 10000 // Default init value.
+	// var sumWords uint
+	for noun, _ := range graph.qaNounToSentences {
+		if graph.NounMaxOccurence < uint(len(graph.qaNounToSentences[noun])) {
+			graph.NounMaxOccurence = uint(len(graph.qaNounToSentences[noun]))
+		}
+		if graph.NounMinOccurence > uint(len(graph.qaNounToSentences[noun])) {
+			graph.NounMinOccurence = uint(len(graph.qaNounToSentences[noun]))
+		}
+		graph.NounSumOccurence += uint(len(graph.qaNounToSentences[noun]))
+	}
+	graph.NounMeanOccurence = float64(float64(graph.NounSumOccurence) / float64(len(graph.Nouns)))
+	graph.NounStdDev = math.Sqrt(float64(graph.NounSumOccurence)/graph.NounMeanOccurence)
+	graph.NounZScore = graph.NounStdDev / float64(graph.NounMaxOccurence-graph.NounMinOccurence)
+	graph.TriGramMinOccurence = 10000
+	for trigramPtr, _ := range graph.qaTriGramToSentences {
+		if len(graph.qaTriGramToSentences[trigramPtr]) == 0 {
+			delete(graph.qaTriGramToSentences, trigramPtr)
+			*trigramPtr = TriGram{}
+			continue
+		}
+		if graph.TriGramMaxOccurence < uint(len(graph.qaTriGramToSentences[trigramPtr])) {
+			graph.TriGramMaxOccurence = uint(len(graph.qaTriGramToSentences[trigramPtr]))
+		}
+		if graph.TriGramMinOccurence > uint(len(graph.qaTriGramToSentences[trigramPtr])) {
+			graph.TriGramMinOccurence = uint(len(graph.qaTriGramToSentences[trigramPtr]))
+		}
+		graph.TriGramSumOccurence += uint(len(graph.qaTriGramToSentences[trigramPtr]))
+	}
+	graph.TriGramMeanOccurence = float64(float64(graph.TriGramSumOccurence) / float64(len(graph.qaTriGramToSentences)))
+		graph.TriGramStdDev = math.Sqrt(float64(graph.TriGramSumOccurence) / graph.TriGramMeanOccurence)
+	graph.TriGramZScore = graph.TriGramStdDev / float64(graph.TriGramMaxOccurence-graph.TriGramMinOccurence)
+}
